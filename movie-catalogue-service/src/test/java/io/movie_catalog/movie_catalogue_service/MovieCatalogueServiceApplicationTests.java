daf3e7310d8485df65dd16ce9fa8623b63707b69
mayhim182 <mayankatnitrkl@gmail.com>
2024-09-22 13:12:50 +0530

    MS1

---
+++ movie-catalogue-service/src/test/java/io/movie_catalog/movie_catalogue_service/MovieCatalogueServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.movie_catalog.movie_catalogue_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogueServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
